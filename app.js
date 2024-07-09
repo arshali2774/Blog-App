836c98083c2cb49f0b7dd04a6a41834629ca5240
Arsh Ali <arshaliWORK@gmail.com>
2024-07-09 20:47:04 +0530

    Added boilerplate code to the app.js file, created server, public and views folder. The server folder currently contains the routes folder and route folder contains a main.js file which is the main route file. The public folder contains all 3 folders currently js, css and images. The views folder contains the layouts folder and the index.ejs file. The index.ejs file is the homepage of our blog and the layouts folder contains the main.ejs file which will be used as the common layout between pages.

---
+++ app.js
@@ -0,0 +1,27 @@
+// importing dotenv
+import dotenv from 'dotenv';
+dotenv.config();
+
+// importing all the packages
+import express from 'express';
+import expressLayouts from 'express-ejs-layouts';
+
+// importing routes
+import router from './server/routes/main.js';
+
+const app = express(); // creating express app
+const PORT = process.env.PORT || 3000; // creating port
+
+app.use(express.static('public')); // serving static files
+
+// templating engine
+app.set('view engine', 'ejs'); // setting view engine
+app.set('layout', './layouts/main'); // setting layout
+app.use(expressLayouts); // using express layouts
+
+app.use('/', router); // using router
+
+// starting server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- readme.md
+++ readme.md
@@ -15,3 +15,29 @@ NPM Packages used for this project
 Development dependencies
 
 - nodemon
+
+Tools used
+
+- vscode
+- postman
+- mongodb compass
+
+Database used
+
+- mongodb
+
+ODM used
+
+- mongoose
+
+Journal
+
+1. Installed all the packages
+2. Created .env file
+3. Boilerplate code for app.js like, setting port, static files, templating engine, using express layouts, using router, starting server.
+4. Created server folder and routes folder inside it, then added main.js inside routes folder.
+5. Created views folder with layouts directory and an index.ejs file inside it.
+6. Layouts folder has a main.ejs file inside it.
+7. Added HTML to main.ejs file in layouts folder.
+8. Added `<%- body %>` to main.ejs file to render the body of the page.
+9.
